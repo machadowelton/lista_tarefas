14b097948aa1a7e60ab3f5b88d017a8c38221be7
Welton Leao Machado <wlmachado@bbmapfre.com.br>
2019-12-03 08:57:12 -0300

    Adicionando deletar item, desfazer e ordernar por não concluidos primeiro

--- lib/main.dart
+++ lib/main.dart
@@ -46,44 +46,87 @@ class _HomeState extends State<Home> {
 
   final _toDoController = TextEditingController();
   List _toDoList = [];
+
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovePos;
+
   void _addToDo() {
+    if (_toDoController.text.isNotEmpty) {
+      setState(() {
+        Map<String, dynamic> newToDo = Map();
+        newToDo["title"] = _toDoController.text;
+        _toDoController.clear();
+        newToDo["ok"] = false;
+        _toDoList.add(newToDo);
+        _saveData();
+      });
+    }
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
     setState(() {
-      Map<String, dynamic> newToDo = Map();
-      newToDo["title"] = _toDoController.text;
-      _toDoController.clear();
-      newToDo["ok"] = false;
-      _toDoList.add(newToDo);
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
       _saveData();
     });
   }
 
   Widget buildItem(context, index) {
     return Dismissible(
-        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-        background: Container(
-          color: Colors.red,
-          child: Align(
-            alignment: Alignment(-0.9, 0.0),
-            child: Icon(
-              Icons.delete,
-              color: Colors.white,
-            ),
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
           ),
         ),
-        direction: DismissDirection.startToEnd,
-        child: CheckboxListTile(
-          title: Text(_toDoList[index]["title"]),
-          value: _toDoList[index]["ok"],
-          secondary: CircleAvatar(
-            child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.warning),
-          ),
-          onChanged: (value) {
-            setState(() {
-              _toDoList[index]["ok"] = value;
-              _saveData();
-            });
-          },
-        ));
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.warning),
+        ),
+        onChanged: (value) {
+          setState(() {
+            _toDoList[index]["ok"] = value;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovePos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+          final snack = SnackBar(
+            content: Text("Tarefa: \"${_lastRemoved["title"]}\" removida"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovePos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
   }
 
   @override
@@ -118,12 +161,14 @@ class _HomeState extends State<Home> {
               ),
             ),
             Expanded(
+                child: RefreshIndicator(
+              onRefresh: _refresh,
               child: ListView.builder(
                 padding: EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
                 itemBuilder: buildItem,
               ),
-            )
+            ))
           ],
         ));
   }
